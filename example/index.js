5156ec4824fba8944c6ba00491a614035a35c44e
Jin IGARASHI <wushilan0623@gmail.com>
2020-09-21 21:48:22 +0900

    added github ribbon

--- example/index.js
+++ example/index.js
@@ -19,4 +19,11 @@ $(function(){
         {title: "Kilgoris",latlng: [34.87533, -1.00278],zoom: 14}, 
         {title: "Suswa",latlng: [36.33078, -1.05307],zoom: 13}
     ], 5), 'top-right');
+
+    map.addControl(new MapboxAreaSwitcherControl([
+        {title: 'Narok',latlng: [35.87063, -1.08551],zoom: 14,}, 
+        {title: "Ololulung'a",latlng: [35.65072, -1.0085],zoom: 13}, 
+        {title: "Kilgoris",latlng: [34.87533, -1.00278],zoom: 14}, 
+        {title: "Suswa",latlng: [36.33078, -1.05307],zoom: 13}
+    ], 3), 'top-right');
 })
